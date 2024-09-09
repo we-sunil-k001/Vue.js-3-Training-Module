eae4c81fa0422f14aaf17deb8ee1f75b6ea41033
Sunil Kumar <sunil.kumar@toolstation.com>
2024-09-09 11:11:16 +0530

    Added: Practiced all the concepts in this chapter
    Refs and Reactive, Computed Property, Class Bindings, Binding Inline Styles using :style attribute, Directives v-if, v-else, v-else-if, v-show, v-for Directive

---
+++ template-syntax/template-syntax/src/components/ClassComponent.vue
@@ -0,0 +1,43 @@
+<template>
+#Class component
+<ol>
+    <li> <h3 :class="{ 'mytitle': isActive, 'text-danger':someVariable }"> way - Hello this is Class component - using ref</h3></li>
+    <li> <h3 :class="classObject"> way - Class component using reactive-object</h3></li>
+    <li> <h3 :class="computedClassObj"> way - Class component using computed</h3></li>
+    <li> <h3 :class="['mytitle',{'text-danger': someVariable}]"> way - Class component using Array</h3></li>
+
+    <li>
+        <!-- Binding Multiple Classes (Static + Dynamic) -->
+        <h3 class="mytitle" :class="{ active: isActive, disabled: !isActive }">
+            Hello, this is class binding with static and dynamic classes
+        </h3>
+    </li>
+    <li>There few more ways...</li>
+</ol>
+    
+</template>
+
+<script setup>
+    import {reactive, ref, computed} from 'vue';
+
+    // 1st way
+    const isActive = ref(true);
+    const someVariable = ref(false);
+
+
+    // 2nd way 
+    const classObject = reactive({
+         'mytitle': true, 
+         'text-danger':true 
+         });
+
+
+    // 3rd way
+    const computedClassObj = computed(()=>{
+        return {
+            'mytitle': isActive.value && someVariable.value,        // checks variable True or false for both the variable
+            'text-danger': isActive.value                           // check for isActive variable only
+        }
+    });
+    
+</script>
---
+++ template-syntax/template-syntax/src/components/ListRenderingComponent.vue
@@ -0,0 +1,33 @@
+<template>
+#ListRenderingComp
+
+<ol>
+    <li v-for="employee in employees" :key="employee.id">
+      {{ employee.name }} - {{ employee.position }}
+    </li>
+
+     <li v-for="(value, key) in user" :key="key">
+      {{ key }}: {{ value }}
+    </li>
+</ol>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// Rendering a Range of Numbers
+const employees = ref([
+  { id: 1, name: 'John', position: 'Manager' },
+  { id: 2, name: 'Jane', position: 'Developer' },
+  { id: 3, name: 'Sam', position: 'Designer' }
+]);
+
+
+// Rendering an Object's Properties
+const user = reactive({
+  name: 'John Doe',
+  age: 30,
+  occupation: 'Developer'
+});
+
+</script>
