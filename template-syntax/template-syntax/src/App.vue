e17b73b897ea6946f342bffc9370009eef7eef7e
Sunil Kumar <sunil.kumar@toolstation.com>
2024-09-11 05:26:23 +0530

    Updated: Minor fixes related to develop and Main Branch

--- template-syntax/template-syntax/src/App.vue
+++ template-syntax/template-syntax/src/App.vue
@@ -1,6 +1,18 @@
 
 <template>
-
+ <counter></counter>
+  <hr>
+  <TemplateSyntax></TemplateSyntax>
+  <hr>
+  <ComputedComp></ComputedComp>
+  <hr>
+  <ClassComp></ClassComp>
+  <hr>
+  <StyleComp></StyleComp>
+  <hr>
+  <ConditionalComp></ConditionalComp>
+  <hr>
+  <ListRenderingComp></ListRenderingComp>
   <hr>
   <EventHandlingComp></EventHandlingComp>
   <hr>
