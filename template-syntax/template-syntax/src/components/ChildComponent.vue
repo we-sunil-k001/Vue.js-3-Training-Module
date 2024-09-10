8dd247341f9397d96bbe376b8c362fdd92ef861d
Sunil Kumar <sunil.kumar@toolstation.com>
2024-09-10 18:38:53 +0530

    Updated: Pass data from parent to child comp.

--- template-syntax/template-syntax/src/components/ChildComponent.vue
+++ template-syntax/template-syntax/src/components/ChildComponent.vue
@@ -1,8 +1,32 @@
 <template>
-  <div>
-    <p>Hello from Child Component!</p>
-  </div>
+#Child Component
+<ol>
+    <li>
+        <div>
+            <p>Hello from Child Component!!!</p>
+        </div>
+    </li>
+    <li>
+        title reveived from parent comp.: {{title}}
+    </li>
+    <li>
+        ID reveived from parent comp.: {{id}}
+    </li>
+
+</ol>
+ 
 </template>
 
 <script setup>
+// get data from another component
+// 1st way
+// const props = defineProps(['title']);
+
+//2nd way
+const props = defineProps({
+    title: String,
+    id: Number
+});
+
+console.log(props.title);
 </script>
--- template-syntax/template-syntax/src/components/ParentComponent.vue
+++ template-syntax/template-syntax/src/components/ParentComponent.vue
@@ -2,7 +2,8 @@
 #Component Registeration
   <div>
     <h2>Parent Component</h2>
-    <ChildComponent />
+    <ChildComponent title="Hello world" />   <!-- Passing data to child component-->
+    <ChildComponent :title="childDetails.title" :id="childDetails.id"/>   <!-- Passing data to child component-->
   </div>
 </template>
 
@@ -10,4 +11,8 @@
 // Register the child component locally
 import ChildComponent from './ChildComponent.vue' // Import the child component
 
+const childDetails = {
+  title: "hello world",
+  id: 21
+};
 </script>
