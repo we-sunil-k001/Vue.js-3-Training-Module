483c62954b8c79859d1ee5a2b8cbdca29d5aab40
Sunil Kumar <sunil.kumar@toolstation.com>
2024-09-11 08:09:18 +0530

    Added: Learnt Child to Parent Communication using Custom Event

---
+++ template-syntax/template-syntax/src/components/Post.vue
@@ -0,0 +1,28 @@
+<template>
+<h4>#Child to Parent Communication using Custom Event</h4>
+<ol>
+    <li>
+        <h3>Post Details</h3>
+        <SinglePost v-for="post in posts" :post="post" :key="post.id" @edit-post="updatePost"></SinglePost>
+    </li>
+</ol>
+</template>
+
+<script setup>
+import SinglePost from './SinglePost.vue';
+import {ref} from 'vue';
+
+const posts = ref([
+    {id:1, title: 'Post 1'},
+    {id:2, title: 'Post 2'},
+    {id:3, title: 'Post 3'},
+]);
+
+const updatePost = (postId) => {
+    posts.value = posts.value.map((post)=>{
+        if(post.id ==postId) return {post, title:'Post is updated'};
+
+        return post;
+    });
+}
+</script>
---
+++ template-syntax/template-syntax/src/components/SinglePost.vue
@@ -0,0 +1,16 @@
+<template>
+    <!-- <h3>#Child to Parent Communication using Custom Event - SinglePost</h3> -->
+  
+    <div>ID: {{post.id}}</div>
+    <div>Title: {{post.title}}</div>
+    <div><button @click.prevent="$emit('editPost', post.id)">Edit Post</button></div>
+</template>
+
+<script setup>
+const props = defineProps({
+    post:{
+        type: Object,
+        required: true
+    }
+});
+</script>
