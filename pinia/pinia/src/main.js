1c86ef27a45ae4a2c97295c65050bfef30d2bfc5
Sunil Kumar <sunil.kumar@toolstation.com>
2024-09-12 10:26:25 +0530

    Updated: Installing Pinia & Making a Store

--- pinia/pinia/src/main.js
+++ pinia/pinia/src/main.js
@@ -1,6 +1,13 @@
 import './assets/main.css'
+import { createPinia } from 'pinia';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(createPinia()).mount('#app')
+
+// OR===============
+
+// import { createPinia } from 'pinia';
+// const pinia = createPinia();
+// app.use(pinia);
